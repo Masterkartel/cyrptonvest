581bc258063ddba701a09498649fdd5fc2885475
LEONARD KIPCHIRCHIR <109794940+Masterkartel@users.noreply.github.com>
2025-09-16 18:22:26 +0300

    Update me.ts

--- functions/api/me.ts
+++ functions/api/me.ts
@@ -1,6 +1,7 @@
-import { json } from "../_utils";
+import { json, requireAuth, type Env } from "../_utils";
 
-export const onRequestGet: PagesFunction = async ({ data }) => {
-  if (!data.user) return new Response("Unauthorized", { status: 401 });
-  return json({ user: data.user });
+export const onRequestGet: PagesFunction<Env> = async ({ env, request }) => {
+  const g = await requireAuth(env, request);
+  if (!g.ok) return g.res;
+  return json({ user: g.user });
 };
